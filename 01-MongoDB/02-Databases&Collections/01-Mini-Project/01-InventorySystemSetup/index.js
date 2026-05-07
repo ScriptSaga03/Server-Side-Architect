27560212b83b3fbf7de42338b0c83ad383b59b5c
Mehtab <mehtab185198131@gmail.com>
2026-05-07 23:28:13 +0530

    Modify sales collection in inventoryDB
    Removed and recreated the 'sales' collection in the inventoryDB.

--- 01-MongoDB/02-Databases&Collections/01-Mini-Project/01-InventorySystemSetup/index.js
+++ 01-MongoDB/02-Databases&Collections/01-Mini-Project/01-InventorySystemSetup/index.js
@@ -1,18 +1,1 @@
-// create a db inventoryDB
-use inventoryDB;
 
-// add collections
-db.createCollection('products');
-db.createCollection('suppliers');
-db.createCollection('sales');
-
-// confirm collections exist
-show collections;
-
-
-// drop sales collections
-db.sales.drop();
-show collections;
-
-// Recreate it again 
-db.createCollection('sales');
