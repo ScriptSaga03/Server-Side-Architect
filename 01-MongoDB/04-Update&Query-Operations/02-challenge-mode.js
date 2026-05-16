5df56bdb443ea85262d09b360ffe05c2d11bdfb0
Mehtab <mehtab185198131@gmail.com>
2026-05-16 19:46:01 +0530

    Add MongoDB queries for employee filtering

--- 01-MongoDB/04-Update&Query-Operations/02-challenge-mode.js
+++ 01-MongoDB/04-Update&Query-Operations/02-challenge-mode.js
@@ -22,3 +22,41 @@ db.employees.find({"projects.status" : "ongoing"}, {_id:0, name:1, projects:1}).
 db.employees.find({skills:  {$all:  ["JavaScript", "React"]}}, {_id:0, name:1, skills:1}).pretty();
 // Find employees who have more than 1 project.
 db.employees.find({$expr : {$gt: [ {$size: '$projects'}, 1]}}, {_id:0}).pretty();
+
+
+
+
+// Combined Operators
+
+// Find active Developers with salary > 45000 and age < 30.
+db.employees.find(
+    {
+        position: "Developer",
+        salary: {$gt : 45000},
+        age: {$lt : 30}
+    },
+    {_id:0, name:1, age:1, salary:1}
+).pretty();
+
+
+// Find employees whose name starts with "J" or who are Managers.
+// Definition: Using the $or operator array to combine a position match with a ^J case-insensitive regular expression for name evaluation.
+db.employees.find(
+    {
+        $or: [
+            { position: 'Manager' },
+            { name: { $regex: /^J/, $options: 'i' } }
+        ]
+    },
+    { name: 1, position: 1, _id: 0 }
+).pretty();
+
+
+// Find employees with skills containing "Leadership" but not active.
+db.employees.find(
+    {
+        active: false,
+        skills: "Leadership"
+    },
+    { _id: 0, name: 1, skills: 1, active: 1 }
+).pretty();
