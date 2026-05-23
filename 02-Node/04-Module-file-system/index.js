36db92a25b3524b78c517879e765c1cb8051043d
Mehtab <mehtab185198131@gmail.com>
2026-05-23 14:21:07 +0530

    Update index.js

--- 02-Node/04-Module-file-system/index.js
+++ 02-Node/04-Module-file-system/index.js
@@ -1,12 +1,31 @@
 // file operations
-import {writeData, readData} from './fileOperations.js'
+// import {writeData, readData} from './fileOperations.js'
+
+// async function main(){
+//    await writeData();
+//   await appendData();
+//   await readData();
+
+//   await deleteFile();
+// }
+
+
+
+import {writeData, readData,  createFolder} from './fileOperations.js'
+const userContent = {
+  id: "A0W" + Math.floor(100 + Math.random() * 900),
+  name:'mehtab',
+  role:'backend developer',
+  status:'Learning Node.js 🚀',
+  loginAt: new Date().toLocaleTimeString()
+}
+
 
 async function main(){
-   await writeData();
-  await appendData();
+  await createFolder();
+  await writeData(userContent);
   await readData();
-
-  await deleteFile();
 }
+
 
 main();
