6fd56a19694c45c0b882925b847f370741064645
Mehtab <mehtab185198131@gmail.com>
2026-05-24 16:01:03 +0530

    Create a simple HTTP server using Node.js

--- 02-Node/02-create-server/index.js
+++ 02-Node/02-create-server/index.js
@@ -1,1 +1,19 @@
+// Import the http module
+import http from "http";
 
+
+// Define the port number
+const PORT = 3000;
+
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });  // send HTTP status and headers
+  res.write("Hello, World! This is a simple HTTP server."); // send response body
+  res.end(); // end the response
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`🚀 Server is running on port http://localhost:${PORT}`);
+});
