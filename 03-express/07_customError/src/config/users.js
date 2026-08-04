6b429d285bab26c0523d25a28741126a0df6afce
Mehtab <mehtab185198131@gmail.com>
2026-08-04 13:59:23 +0530

    Add files via upload

---
+++ 03-express/07_customError/src/config/users.js
@@ -0,0 +1,19 @@
+
+// data
+const users = [
+  {
+    "id": 1,
+    "name": "Rahul Sharma",
+    "email": "rahul@gmail.com",
+    "password": "123456"
+  },
+  {
+    "id": 2,
+    "name": "Priya Verma",
+    "email": "priya@gmail.com",
+    "password": "password123"
+  }
+]
+
+
+export default users;
