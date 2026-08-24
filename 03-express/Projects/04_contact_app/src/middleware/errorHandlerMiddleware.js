d6d8e21c90120f1d085d04bd230393697e609f9f
Mehtab <mehtab185198131@gmail.com>
2026-08-24 16:44:06 +0530

    Add files via upload

---
+++ 03-express/Projects/04_contact_app/src/middleware/errorHandlerMiddleware.js
@@ -0,0 +1,18 @@
+
+
+
+const centralizedErrorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || err.status || 500;
+    const message = err.message || `❌ Internal server error!`;
+    const status = statusCode >= 400 && statusCode < 500 ? "Fail" : "Error";
+
+
+    return res.status(statusCode).json({
+        success: false,
+        status,
+        statusCode,
+        message
+    })
+};
+
+export default centralizedErrorHandler; 
